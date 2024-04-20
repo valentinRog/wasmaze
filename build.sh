c201c0ca1dea48374c19f0d3c677ea3708867375
valentin <valentin.rogister@gmail.com>
2024-04-20 03:50:40 +0200

    have a maze

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-em++ -I include *.cpp src/**.cpp -o dist/game.js -s WASM=1 -s -lembind -s ALLOW_MEMORY_GROWTH=1
+em++ -std=c++20 -I include *.cpp src/**.cpp -o dist/game.js -s WASM=1 -s -lembind -s ALLOW_MEMORY_GROWTH=1
--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,12 @@
 #include "api.h"
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <iostream>
 #include <map>
+#include <random>
 #include <set>
+#include <stack>
 
 namespace game {
 struct Vec2 {
@@ -30,20 +36,65 @@ struct Screen {
 static Screen screen;
 static Maze maze;
 
+static void make_maze() {
+  for (auto &[_, v] : maze.conns) {
+    v.clear();
+  }
+
+  std::stack<Vec2> st;
+  st.push(Vec2{0, 0});
+  std::set<Vec2> visited;
+  visited.insert(Vec2{0, 0});
+
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+
+  while (st.size()) {
+    auto p = st.top();
+    std::array<Vec2, 4> nps = {Vec2{p.x + 1, p.y}, Vec2{p.x - 1, p.y},
+                               Vec2{p.x, p.y + 1}, Vec2{p.x, p.y - 1}};
+    std::shuffle(nps.begin(), nps.end(), gen);
+    bool any = false;
+    for (const auto np : nps) {
+      if (!maze.conns.count(np)) {
+        continue;
+      }
+      if (maze.conns.at(p).count(np)) {
+        continue;
+      }
+      if (visited.count(np)) {
+        continue;
+      }
+      visited.insert(np);
+      st.push(np);
+      maze.conns[p].insert(np);
+      maze.conns[np].insert(p);
+      any = true;
+      break;
+    }
+    if (!any) {
+      st.pop();
+    }
+  }
+}
+
 void init(int width, int height) {
   screen = {.width = width, .height = height};
-  maze.n_cell_x = 40;
+  maze.n_cell_x = 80;
   maze.n_cell_y = maze.n_cell_x * (static_cast<float>(screen.height) /
                                    static_cast<float>(screen.width));
-  maze.cell_w = screen.width / maze.n_cell_x;
-  for (int y(0); y < screen.height; y++) {
-    for (int x(0); x < screen.width; x++) {
+  maze.cell_w =
+      static_cast<float>(screen.width) / static_cast<float>(maze.n_cell_x);
+  for (int y(0); y < maze.n_cell_y; y++) {
+    for (int x(0); x < maze.n_cell_x; x++) {
       maze.conns[Vec2{.x = x, .y = y}];
     }
   }
 }
 
 void render() {
+  make_maze();
+
   for (int y(0); y < maze.n_cell_y; y++) {
     for (int x(0); x < maze.n_cell_x; x++) {
       const auto cw = maze.cell_w;
