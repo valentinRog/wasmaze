4a61f82f54bfa5c27fc5bb22b033af620fae37da
valentin <valentin.rogister@gmail.com>
2024-04-20 18:06:25 +0200

    yo

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-cp *.js dist/
-cp index.html dist/
-cp style.css dist/
 em++ -O3 -std=c++20 -I include *.cpp src/**.cpp -o dist/game.js -s WASM=1 -s -lembind -s ALLOW_MEMORY_GROWTH=1
